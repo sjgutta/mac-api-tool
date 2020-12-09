dbbb535772f5be5501ca3361b208f44945efb4c1
Sajan Gutta <gutta@usc.edu>
2020-12-09 14:15:19 -0500

    creating xcode project

---
+++ Mac API Tool/Mac API ToolTests/Mac_API_ToolTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mac_API_ToolTests.swift
+//  Mac API ToolTests
+//
+//  Created by Sajan  Gutta on 12/9/20.
+//
+
+import XCTest
+@testable import Mac_API_Tool
+
+class Mac_API_ToolTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
