6a69038eed14a4999685d495a6acac7ef671d988
Sajan Gutta <gutta@usc.edu>
2020-12-09 15:41:43 -0500

    creating enum for the request type with one option for each standard REST operation

---
+++ Mac API Tool/Mac API Tool/RequestType.swift
@@ -0,0 +1,15 @@
+//
+//  RequestType.swift
+//  Mac API Tool
+//
+//  Created by Sajan  Gutta on 12/9/20.
+//
+
+import Foundation
+
+enum RequestType: String, CaseIterable {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
