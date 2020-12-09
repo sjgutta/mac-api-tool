8bd171e9e6034644e20657aada37f6ee34793195
Sajan Gutta <gutta@usc.edu>
2020-12-09 18:15:44 -0500

    creating file to hold functions for executing requests, implementing percentEncoding function to be used on Dictionary of parameters

---
+++ Mac API Tool/Mac API Tool/RequestFunctions.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  Mac API Tool
+//
+//  Created by Sajan  Gutta on 12/9/20.
+//
+
+import Foundation
+
+extension Dictionary {
+    func percentEncoded() -> Data? {
+        return map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+        }
+        .joined(separator: "&")
+        .data(using: .utf8)
+    }
+}
+
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
